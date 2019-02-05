f6885555ff8a45073d057be63f784e0013b4f3b2
Admin <Admin@fc.oit>
2019-02-05 17:38:59 +0600

    Adding all class files

---
+++ src/main/Human.java
@@ -0,0 +1,58 @@
+package main;
+
+public class Human {
+	private String firstName;
+	private String lastName;
+	private String patronymic;
+	private String gender;
+	private int age;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPatronymic() {
+		return patronymic;
+	}
+	
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Human [firstName=" + this.firstName 
+				+ ", lastName=" + this.lastName
+				+ ", patronymic=" + this.patronymic
+				+ ", gender=" + this.gender
+				+ ", age=" + this.age + "]";
+	}
+}
---
+++ src/main/Student.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Student extends Human {
+	private int studentID;
+
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName" + this.getFirstName() 
+		+ ", lastName=" + this.getLastName() 
+		+ ", patronymic=" + this.getPatronymic()
+		+ ", gender=" + this.getGender()
+		+ ", age=" + this.getAge()
+		+ ", studentID=" + studentID + "]";
+	}
+}
